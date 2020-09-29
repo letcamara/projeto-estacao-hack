44481f80a5c7c279e8649d2a9afd940996ca365e
Leticia Camara <lettcamara@gmail.com>
2020-09-29 14:10:58 -0300

    Conclusão da estrutura do projeto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+const fieldSearchNameRemedy = document.getElementsByClassName('ph__field-name')
+const fieldUserLocalization = document.getElementsByClassName('ph__field-state')
+const form = document.querySelector('form');
+
+console.log(fieldSearchNameRemedy)
+
+console.log(fieldUserLocalization)
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(fieldSearchNameRemedy.value)
+
+})
 
